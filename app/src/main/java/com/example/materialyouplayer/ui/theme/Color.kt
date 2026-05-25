e50583b3cbf1426128055d095ab2e4492c271472
ymh9430-sys <ymh9430@gmail.com>
2026-05-25 20:08:23 +0300

    Create Color.kt

---
+++ app/src/main/java/com/example/materialyouplayer/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.materialyouplayer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// ألوان الواجهة الـ Minimalist الداكنة المتوافقة مع الـ Material You
+val PureBlack = Color(0xFF000000)
+val MaterialGreen = Color(0xFF34C759) // الأخضر الماتيريال الأنيق للمشغل والـ Navigation
+val DarkGrayBg = Color(0xFF111111)
+val SurfaceColor = Color(0xFF1E1E1E)
