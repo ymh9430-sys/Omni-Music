d6c4a6329974e0a54109b50ad92984d62cdb3d72
ymh9430-sys <ymh9430@gmail.com>
2026-05-25 21:45:06 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation(libs.androidx.media3.ui)
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.customview:customview:1.1.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
